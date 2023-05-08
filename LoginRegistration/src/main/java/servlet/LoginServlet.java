55f2374ae2d203149b95244d3048fa3a57ecdc6e
Diana <125206712+Valainie@users.noreply.github.com>
2023-05-08 16:17:49 +0200

    Add files via upload
    Signed-off-by: Diana <125206712+Valainie@users.noreply.github.com>

---
+++ LoginRegistration/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,35 @@
+package servlet;
+
+import bean.UserBean;
+import controller.UserController;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            String mail = request.getParameter("Email");
+            String p=request.getParameter("Password");
+           UserController controller = new UserController();
+            UserBean bean = controller.login(mail,p);
+            if (bean == null) {
+                response.sendRedirect("invalidLogin.jsp");
+            }
+            else{
+                request.getSession().setAttribute("name", bean.getNome());
+
+            response.sendRedirect("userLogged.jsp");
+            }
+
+        } catch (Throwable exception) {
+            System.out.println(exception);
+        }
+    }
+}
